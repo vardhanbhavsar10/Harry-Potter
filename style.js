d22c484f9b9be0c9967be7de3309859053706091
Vardhan Bhavsar <86793606+Phoenix0209@users.noreply.github.com>
2023-05-02 12:25:40 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,42 @@
+
+// $(function () {
+//   var str = '#len'; //increment by 1 up to 1-nelemnts
+//   $(document).ready(function () {
+//     var i, stop;
+//     i = 1;
+//     stop = 4; //num elements
+//     setInterval(function () {
+//       if (i > stop) {
+//         return;
+//       }
+//       $('#len' + (i++)).toggleClass('bounce');
+//     }, 500)
+//   });
+// });
+
+// $(window).scroll(function() {
+//   var height = $(window).scrollTop();
+//   if(height > 100) {
+//     $('.navbar').addClass('bounce');
+//   } else {
+//     $('.navbar').removeClass('bounce');
+//   }
+// });
+
+// // $(window).scroll(function() {
+// //   if ($(window).scrollTop() > 100) {
+// //     $('.col-md-6 h2, .col-md-6 p').addClass('fixed');
+// //   } else {
+// //     $('.col-md-6 h2, .col-md-6 p').removeClass('fixed');
+// //   }
+// // });
+
+// $(window).scroll(function() {
+//   $('.text-scroll').each(function() {
+//     var position = $(this).offset().top;
+//     var windowTop = $(window).scrollTop();
+//     if (position < windowTop + $(window).height() - 50) {
+//       $(this).addClass('visible');
+//     }
+//   });
+// });
